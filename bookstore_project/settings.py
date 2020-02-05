e5544a96c39c37a53055d86b2585451ef53adae7
rishav <palchaudhuri.rishav@gmail.com>
2020-02-06 01:33:18 +0530

    ch12

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -167,5 +167,7 @@ ACCOUNT_UNIQUE_EMAIL = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
 
-
+# Image upload settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
